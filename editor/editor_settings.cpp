ae4241daae5413e8374a00b5da09359a5c395c30
illlustr <still.lkmn@gmail.com>
2024-06-20 20:36:48 +0700

    change default grid color

--- editor/editor_settings.cpp
+++ editor/editor_settings.cpp
@@ -604,7 +604,7 @@ void EditorSettings::_load_defaults(Ref<ConfigFile> p_extra_config) {
 	_initial_set("editors/3d/freelook/freelook_speed_zoom_link", false);
 
 	// 2D
-	_initial_set("editors/2d/grid_color", Color(1.0, 1.0, 1.0, 0.07));
+	_initial_set("editors/2d/grid_color", Color(0.5, 0.5, 0.5, 0.07));
 	_initial_set("editors/2d/guides_color", Color(0.6, 0.0, 0.8));
 	_initial_set("editors/2d/smart_snapping_line_color", Color(0.9, 0.1, 0.1));
 	_initial_set("editors/2d/bone_width", 5);
