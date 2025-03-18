67b95075f1c86c1dee68d1c6fe57fb6ffdf48590
illlustr <still.lkmn@gmail.com>
2025-03-18 11:14:34 +0700

    update version

--- version.py
+++ version.py
@@ -1,10 +1,10 @@
-short_name = "godot"
-name = "Godot Engine"
+short_name = "Gdx"
+name = "Gdx"
 major = 3
 minor = 7
 patch = 0
 status = "dev"
 module_config = ""
 year = 2024
-website = "https://godotengine.org"
+website = "https://github.com/naiiveprojects"
 docs = "3.6"
